1fdc39b9e0ad2cc781ce16a0a9c3cb2d037a3db9
Joe Farrington <farrington.joe@gmail.com>
2025-02-01 22:41:51 +0000

    Update conf.py
    Correct version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 project = "MDPax"
 copyright = "2024, Joseph Farrington"
 author = "Joseph Farrington"
-release = "0.0.1"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
